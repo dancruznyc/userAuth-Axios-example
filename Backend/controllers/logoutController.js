bb9e9a21e16294123d4e9c2b5c73720235633c3e
dancruznyc <dancruznyc@gmail.com>
2022-04-29 00:30:04 -0400

    cookies, credentials and backend logout

---
+++ Backend/controllers/logoutController.js
@@ -0,0 +1,50 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const handleLogout = async (req, res) => {
+  // On Client, also delete the accessToken
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //successful req, No content
+  console.log(cookies.jwt);
+  const refreshToken = cookies.jwt;
+
+  // is refreshtoken in db?
+  const foundUser = usersDB.users.find(
+    (person) => person.refreshToken === refreshToken
+  );
+  if (!foundUser) {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    return res.sendStatus(204);
+  }
+
+  // Delete refresh form db
+  const otherUsers = usersDB.users.filter(
+    (person) => person.refreshToken !== foundUser.refreshToken
+  );
+  const currentUser = { ...foundUser, refreshToken: "" };
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "model", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  }); // in production also want to add flag secure: true ... for https
+  res.send(204);
+};
+
+module.exports = { handleLogout };
