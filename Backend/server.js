f5ac8885b9073e4d8d368980508e0f1d5b1d5198
dancruznyc <dancruznyc@gmail.com>
2022-04-28 21:15:03 -0400

    userAuth routes backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
@@ -9,23 +10,7 @@ const PORT = process.env.PORT || 3500;
 // custom middleware logger
 app.use(logger);
 
-//cross origin resource sharing
-const whitelist = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
+// Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
 //built-in middleware to handle urlencoded data
@@ -37,51 +22,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //built-in middleware static files
-app.use(express.static(path.join(__dirname, "./public")));
+app.use("/", express.static(path.join(__dirname, "./public")));
 
-app.get("^/$|/index(.html)?", (req, res) => {
-  // res.send("Hello World");
-  // res.sendFile("./views/index.html", { root: __dirname });
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html"); // 302 by default
-});
-
-//Route handlers
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello World");
-  }
-);
-
-// chaining route handlers
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res, next) => {
-  console.log("three");
-  res.send("Finished!");
-};
-
-app.get("/chain(.html)?", [one, two, three]);
+// routes
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/employees", require("./routes/api/employees"));
 
 // app.use('/') does not accept regex
 // app.all() does accept regex more likely to be used for routing
